7ee9734f3f9d7ab3af99a09ad106e47d852b030f
pybites <pybites@projects.bobbelderbos.com>
2016-12-24 15:38:20 +0100

    html is nice for email, but not to copy+paste to FB post, so added a text option. not using arg parser because it is a flag for cronjob, just sys.argv

--- pybites_digest/digest.py
+++ pybites_digest/digest.py
@@ -10,11 +10,15 @@ DEFAULT_DAYS_BACK = 7
 FEEDS = 'http://pybit.es/feeds'
 RSS = 'all.rss.xml'
 SECS_IN_DAY = 24 * 60* 60
-TITLE = '<h1>PyBites weekly digest</h1>'
 ARTICLE_HTML = '''<article>
     <h2><a href="{}">{}</a></h2>
     {}
 </article>'''
+ARTICLE_TXT = '''{}
+{}
+{}
+--
+'''
 
 def calc_utstamp_limit(days_back):
     now = int(time.time())
@@ -23,20 +27,26 @@ def calc_utstamp_limit(days_back):
 def get_articles(utstamp_limit):
     xml = RSS if os.path.isfile(RSS) else os.path.join(FEEDS, RSS)
     feed = feedparser.parse(xml)
-    for e in feed['entries']:
-        utstamp = time_to_unix(e['published_parsed'])
+    for article in feed['entries']:
+        utstamp = time_to_unix(article['published_parsed'])
         if utstamp < utstamp_limit:
             continue
-        yield ARTICLE_HTML.format(
-            e['link'], e['title'], e['summary']
-        )
+        yield article
 
 def time_to_unix(t):
     return int(time.mktime(t))
 
+def strip_html(text):
+    return re.sub('<[^<]+?>', '', text)
+
 if __name__ == "__main__":
     days_back = int(sys.argv[1]) if len(sys.argv) > 1 else DEFAULT_DAYS_BACK
+    html = True if len(sys.argv) > 2 else False
     utstamp_limit = calc_utstamp_limit(days_back)
-    print(TITLE)
     for article in get_articles(utstamp_limit):
-        print(article)
+        if html:
+            print(ARTICLE_HTML.format(article['link'], 
+                  article['title'], article['summary']))
+        else:
+            print(ARTICLE_TXT.format(article['title'], 
+                  article['link'], strip_html(article['summary'])))
