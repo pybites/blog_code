301496ee998fe393bf30275fc7fad05426771ce2
pybites <pybites@projects.bobbelderbos.com>
2016-12-26 10:34:54 +0100

    start pylinting script

--- pybites_digest/digest.py
+++ pybites_digest/digest.py
@@ -8,7 +8,7 @@ import feedparser
 DEFAULT_DAYS_BACK = 7
 FEEDS = 'http://pybit.es/feeds'
 RSS = 'all.rss.xml'
-SECS_IN_DAY = 24 * 60* 60
+SECS_IN_DAY = 24 * 60 * 60
 ARTICLE_HTML = '''<article>
     <h2><a href="{}">{}</a></h2>
     {}
@@ -19,9 +19,12 @@ ARTICLE_TXT = '''{}
 --
 '''
 
+
 def calc_utstamp_limit(days_back):
+    ''' calculates unix timesamp from now minus days back '''
     now = int(time.time())
     return now - (days_back * SECS_IN_DAY)
+
 
 def get_articles(utstamp_limit):
     xml = RSS if os.path.isfile(RSS) else os.path.join(FEEDS, RSS)
@@ -32,11 +35,14 @@ def get_articles(utstamp_limit):
             continue
         yield article
 
+
 def time_to_unix(t):
     return int(time.mktime(t))
 
+
 def strip_html(text):
     return re.sub('<[^<]+?>', '', text)
+
 
 if __name__ == "__main__":
     days_back = int(sys.argv[1]) if len(sys.argv) > 1 else DEFAULT_DAYS_BACK
@@ -44,8 +50,9 @@ if __name__ == "__main__":
     utstamp_limit = calc_utstamp_limit(days_back)
     for article in get_articles(utstamp_limit):
         if html:
-            print(ARTICLE_HTML.format(article['link'], 
-                  article['title'], article['summary']))
+            print(ARTICLE_HTML.format(
+                article['link'], article['title'], article['summary']))
         else:
-            print(ARTICLE_TXT.format(article['title'], 
-                  article['link'], strip_html(article['summary'])))
+            html = strip_html(article['summary'])
+            print(ARTICLE_TXT.format(
+                article['title'], article['link'], html))
