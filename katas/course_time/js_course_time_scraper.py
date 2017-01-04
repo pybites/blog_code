9876f968b49745b599e4bc9716802677956c8b46
pybites <pybites@projects.bobbelderbos.com>
2017-01-04 10:18:20 +0100

    removed whitespaces to comply with pep8 (used flake8 vim plugin)

--- katas/course_time/js_course_time_scraper.py
+++ katas/course_time/js_course_time_scraper.py
@@ -9,19 +9,20 @@ HTML_FILE = "content.html"
 MM_SS_SEP = ':'
 SECONDS_IN_MIN = 60
 SECONDS_IN_HOUR = 60 * SECONDS_IN_MIN
-TIME_REGEX = re.compile(r'\(\d+:\d+\)') 
+TIME_REGEX = re.compile(r'\(\d+:\d+\)')
 
 
 def get_all_timestamps():
     with open(HTML_FILE) as f:
-        return TIME_REGEX.findall(f.read()) 
-    
+        return TIME_REGEX.findall(f.read())
+
+
 def calc_duration(durations):
     sum_seconds = 0
     for mm_ss in durations:
         minutes, seconds = mm_ss.strip('()').split(MM_SS_SEP)
         sum_seconds += int(minutes) * SECONDS_IN_MIN + int(seconds)
-    return sum_seconds 
+    return sum_seconds
 
 
 if __name__ == "__main__":
