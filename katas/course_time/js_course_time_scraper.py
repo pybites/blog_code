e45ce53e3407e25a648225829f2086e8a9020011
pybites <pybites@projects.bobbelderbos.com>
2017-01-04 10:16:43 +0100

    extract colon seperator into constant

--- katas/course_time/js_course_time_scraper.py
+++ katas/course_time/js_course_time_scraper.py
@@ -6,6 +6,7 @@
 import re
 
 HTML_FILE = "content.html"
+MM_SS_SEP = ':'
 SECONDS_IN_MIN = 60
 SECONDS_IN_HOUR = 60 * SECONDS_IN_MIN
 TIME_REGEX = re.compile(r'\(\d+:\d+\)') 
@@ -18,7 +19,7 @@ def get_all_timestamps():
 def calc_duration(durations):
     sum_seconds = 0
     for mm_ss in durations:
-        minutes, seconds = mm_ss.strip('()').split(':')
+        minutes, seconds = mm_ss.strip('()').split(MM_SS_SEP)
         sum_seconds += int(minutes) * SECONDS_IN_MIN + int(seconds)
     return sum_seconds 
 
