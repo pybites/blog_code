3c700fe215f7e12a85d70c92fefbd80baf0b4743
pybites <pybites@projects.bobbelderbos.com>
2017-02-21 08:22:51 +0100

    renamed function, added docstrings

--- various/pybites_party.py
+++ various/pybites_party.py
@@ -13,12 +13,18 @@ AGE_DAYS = (TODAY - PYBITES_START).days
 PYBITES = 'PyBites'
 
 def today_is_special_day(age=None):
+    """
+    Returns bool if today is a special day (yearly birthday or n of 100 days
+    """
     if age is None:
         age = AGE_DAYS
     return any(map(lambda x: age % x == 0, SPECIAL_DAY_OFFSETS))
 
 
-def days_till_bday(age=None):
+def days_till_special_day(age=None):
+    """
+    Calculates days till next special day
+    """
     if age is None:
         age = AGE_DAYS
     return min(map(lambda x: x - age % x, SPECIAL_DAY_OFFSETS))
@@ -26,20 +32,26 @@ def days_till_bday(age=None):
 
 if __name__ == "__main__":
     def test_today_is_special_day():
+        """
+        Tests for range of values if special date
+        """
         test_days = (1, 50, 100, 101, 200, 305, 365, 400, 499, 730, 800, 850)
         test_outcomes = (False, False, True, False, True, False,
                         True, True, False, True, True, False)
         for age, outcome in zip(test_days, test_outcomes):
             assert today_is_special_day(age) == outcome, ERROR_MSG.format(age, outcome)
 
-    def test_days_till_bday():
+    def test_days_till_special_day():
+        """
+        Tests for range of values how many days till next special date
+        """
         test_days = (1, 50, 100, 101, 200, 305, 365, 400, 499, 730, 800, 850)
         test_outcomes = (99, 50, 100, 99, 100, 60, 35, 100, 1, 70, 100, 50)
         for age, outcome in zip(test_days, test_outcomes):
-            assert days_till_bday(age) == outcome, ERROR_MSG.format(age, outcome)
+            assert days_till_special_day(age) == outcome, ERROR_MSG.format(age, outcome)
 
     test_today_is_special_day()
-    test_days_till_bday()
+    test_days_till_special_day()
 
     print('{} is {} days old'.format(PYBITES, AGE_DAYS))
     print('Does {} have a birthday today?'.format(PYBITES))
@@ -51,4 +63,4 @@ if __name__ == "__main__":
         email(PYBITES_EMAIL, subject, message)
         print('Yes!')
     else:
-        print('No ... days till next birtday: {}'.format(days_till_bday()))
+        print('No ... days till next birtday: {}'.format(days_till_special_day()))
