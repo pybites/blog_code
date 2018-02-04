76328ec76a8b9b3a19bcc8bb73a84e9ab6718281
pybites <pybites@projects.bobbelderbos.com>
2018-02-04 10:01:57 +0100

    fixed bug and nicer way to solve this one

--- katas/course_time/js_course_time_scraper.py
+++ katas/course_time/js_course_time_scraper.py
@@ -2,32 +2,53 @@
 # js_course_time_scraper.py is a tool to scrape the html of the Watch and Code
 # JS course to see how long the actual course is in total. It's not listed
 # on the course page/site anywhere thus the necessity of this tool.
+#
+# update 4th of Feb 2018: solved bug and added more elegant way = datetime
 
+from datetime import datetime, timedelta
 import re
 
 HTML_FILE = "content.html"
-MM_SS_SEP = ':'
-SECONDS_IN_MIN = 60
-SECONDS_IN_HOUR = 60 * SECONDS_IN_MIN
-TIME_REGEX = re.compile(r'\(\d+:\d+\)')
 
 
 def get_all_timestamps():
     with open(HTML_FILE) as f:
-        return TIME_REGEX.findall(f.read())
+        content = f.read()
+        return re.findall(r'\d+:\d+', content)
 
 
-def calc_duration(durations):
-    sum_seconds = 0
-    for mm_ss in durations:
-        minutes, seconds = mm_ss.strip('()').split(MM_SS_SEP)
-        sum_seconds += int(minutes) * SECONDS_IN_MIN + int(seconds)
-    return sum_seconds
+def calc_duration(timings):
+    total_seconds = 0
+    for mm_ss in timings:
+        minutes, seconds = mm_ss.split(':')
+        total_seconds += int(minutes) * 60 + int(seconds)
+
+    minutes, seconds = divmod(total_seconds, 60)
+    hours, minutes = divmod(minutes, 60)
+
+    return f'{hours}:{minutes}:{seconds}'
+
+
+def calc_duration_improved(timings):
+    start = datetime.now()
+    end = datetime.now()
+
+    for mm_ss in timings:
+        minutes, seconds = mm_ss.split(':')
+        end += timedelta(minutes=int(minutes), seconds=int(seconds))
+
+    return str(end - start)
 
 
 if __name__ == "__main__":
-    video_timings = get_all_timestamps()
-    total_seconds = calc_duration(video_timings)
-    total_hours = float(total_seconds / SECONDS_IN_HOUR)
-    assert str(total_hours) == '6.841944444444445'
-    print('The course takes ' + str(total_hours) + ' hours to complete.')
+    timings = get_all_timestamps()
+
+    # 1. using counting + divmod
+    course_total = calc_duration(timings)
+    assert str(course_total) == '6:50:31'
+
+    # 2. using datetime (nicer)
+    course_total = calc_duration_improved(timings)
+    assert '6:50:31' in course_total
+
+    print(f'The course takes {course_total} to complete')
