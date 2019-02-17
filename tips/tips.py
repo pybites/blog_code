fac6d855f80d849d25e39a2f9c1c32f3525cb440
Bob Belderbos <bobbelderbos@gmail.com>
2019-02-17 08:35:16 +0100

    take tip id cli arg

--- tips/tips.py
+++ tips/tips.py
@@ -1,5 +1,6 @@
 from collections import namedtuple
 from random import choice
+import sys
 from time import sleep
 import urllib.parse
 
@@ -25,33 +26,34 @@ Tip = namedtuple('Tip', 'tip code src')
 
 def retrieve_tips():
     """Grab and parse all tips from https://codechalleng.es/tips
-       returning a list of Tip namedtuples
+       returning a dict of keys: tip IDs and values: Tip namedtuples
     """
     html = requests.get(TIPS_PAGE)
 
     soup = BeautifulSoup(html.text, 'html.parser')
     trs = soup.findAll("tr")
 
-    tips = []
+    tips = {}
 
     for tr in trs:
         tds = tr.find_all("td")
-        td = tds[1]
+        id_ = int(tds[0].text.strip().rstrip('.'))
+        tip_html = tds[1]
 
-        links = td.findAll("a", class_="left")
+        links = tip_html.findAll("a", class_="left")
         share_link = links[0].attrs.get('href')
 
-        pre = td.find("pre")
+        pre = tip_html.find("pre")
         code = pre and pre.text or ''
 
         # skip if tweeted or not code in tip
         if PYBITES_HAS_TWEETED in share_link or not code:
             continue
 
-        tip = td.find("blockquote").text
+        tip = tip_html.find("blockquote").text
         src = len(links) > 1 and links[1].attrs.get('href') or ''
 
-        tips.append(Tip(tip, code, src))
+        tips[id_] = Tip(tip, code, src)
 
     return tips
 
@@ -80,7 +82,15 @@ def get_carbon_image(tip):
 
 if __name__ == '__main__':
     tips = retrieve_tips()
-    tip = choice(tips)
+    if len(sys.argv) == 2:
+        tip_id = int(sys.argv[1])
+    else:
+        tip_id = choice(list(tips.keys()))
+
+    tip = tips.get(tip_id)
+    if tip is None:
+        print(f'Could not retrieve tip ID {tip_id}')
+        sys.exit(1)
 
     src = tip.src and f' - see {tip.src}' or ''
     img = get_carbon_image(tip)
