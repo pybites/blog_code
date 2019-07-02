5a0dbaddeab47c84101f70ba518994fdc1a052a4
hobojoe1848 <sequeira.julian@gmail.com>
2019-07-02 21:52:02 +1000

    pushing generic emailer code for Flake8 article

---
+++ generic_emailer/generic_emailer.py
@@ -0,0 +1,45 @@
+#!python3
+#emailer.py is a simple script for sending emails using smtplib
+#The idea is to assign a web-scraped file to the DATA_FILE constant.
+#The data in the file is then read in and sent as the body of the email.
+
+import smtplib
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+
+from email_list import EMAILS
+
+DATA_FILE = 'scraped_data_file'
+from_addr = 'your_email@gmail.com'
+to_addr = 'your_email@gmail.com'  #Or any generic email you want all recipients to see
+bcc = EMAILS
+
+msg = MIMEMultipart()
+msg['From'] = from_addr
+msg['To'] = to_addr
+msg['Subject'] = 'Subject Line'
+
+with open(DATA_FILE) as f:
+    body = f.read()
+
+msg.attach(MIMEText(body, 'plain'))
+
+smtp_server = smtplib.SMTP('smtp.gmail.com', 587) #Specify Gmail Mail server
+
+smtp_server.ehlo() #Send mandatory 'hello' message to SMTP server
+
+smtp_server.starttls() #Start TLS Encryption as we're not using SSL.
+
+#Login to gmail: Account | Password
+smtp_server.login(' your_email@gmail.com ', ' GMAIL APPLICATION ID ')
+
+text = msg.as_string()
+
+#Compile email list: From, To, Email body
+smtp_server.sendmail(from_addr, [to_addr] + bcc, text)
+
+#Close connection to SMTP server
+smtp_server.quit()
+
+#Test Message to verify all passes
+print('Email sent successfully')
