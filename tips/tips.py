029d26ef3ae44499a03c50a1f8b01d87a87ba1a4
Bob Belderbos <bobbelderbos@gmail.com>
2020-06-29 11:51:44 +0200

    use api endpoint

--- tips/tips.py
+++ tips/tips.py
@@ -4,11 +4,10 @@ import sys
 from time import sleep
 import urllib.parse
 
-from bs4 import BeautifulSoup
 import requests
 from selenium import webdriver
 
-TIPS_PAGE = 'https://codechalleng.es/tips/all'
+TIPS_PAGE = 'https://codechalleng.es/api/tips'
 PYBITES_HAS_TWEETED = 'pybites/status'
 CARBON = 'https://carbon.now.sh/?l=python&code={code}'
 TWEET = '''{tip} {src}
@@ -20,39 +19,28 @@ TWEET = '''{tip} {src}
 {img}
 '''
 
-Tip = namedtuple('Tip', 'tip code src')
+Tip = namedtuple('Tip', 'tip code link')
 
 
 def retrieve_tips():
     """Grab and parse all tips from https://codechalleng.es/tips
        returning a dict of keys: tip IDs and values: Tip namedtuples
     """
-    html = requests.get(TIPS_PAGE)
-
-    soup = BeautifulSoup(html.text, 'html.parser')
-    trs = soup.findAll("tr")
-
+    resp = requests.get(TIPS_PAGE)
+    resp.raise_for_status()
     tips = {}
 
-    for tr in trs:
-        tds = tr.find_all("td")
-        id_ = int(tds[0].text.strip().rstrip('.'))
-        tip_html = tds[1]
-
-        links = tip_html.findAll("a", class_="left")
-        share_link = links[0].attrs.get('href')
-
-        pre = tip_html.find("pre")
-        code = pre and pre.text or ''
-
-        # skip if tweeted or not code in tip
-        if PYBITES_HAS_TWEETED in share_link or not code:
+    for entry in resp.json():
+        # skip tips that were already shared
+        if entry['share_link'] is not None:
             continue
 
-        tip = tip_html.find("blockquote").text
-        src = len(links) > 1 and links[1].attrs.get('href') or ''
+        idx = entry['id']
+        tip = entry['tip']
+        code = entry['code']
+        link = entry['link']
 
-        tips[id_] = Tip(tip, code, src)
+        tips[idx] = Tip(tip=tip, code=code, link=link)
 
     return tips
 
@@ -91,7 +79,7 @@ if __name__ == '__main__':
         print(f'Could not retrieve tip ID {tip_id}')
         sys.exit(1)
 
-    src = tip.src and f' - see {tip.src}' or ''
+    src = f' - see {tip.link}' if tip.link else ''
     img = get_carbon_image(tip)
 
     tweet = TWEET.format(tip=tip.tip, src=src, img=img)
